7bda0f3da7416a84f83a5e3e5803ceedfaf1b0cc
BaseInfinity <stefanayala3266@gmail.com>
2015-04-01 18:17:27 -0700

    Add tmux info

--- quickInstall.sh
+++ quickInstall.sh
@@ -2,4 +2,10 @@
 
 cp vimrc ~/.vimrc
 cp bashrc ~/.bashrc
+cp tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.vim/colors
+mkdir -p ~/.themes/solarized/tmux-colors-solarized
+
 cp solarized.vim ~/.vim/colors/solarized.vim
+cp tmuxcolors-dark.conf ~/.themes/solarized/tmux-colors-solarized/tmuxcolors-dark.conf 
